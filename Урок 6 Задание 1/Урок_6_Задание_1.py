f42bf52a1b5e987469da9ed76699738d35ed46b2
pasha <pasha@HONOR1>
2024-10-25 15:38:25 +0400

    Добавьте файлы проекта.

---
+++ Урок 6 Задание 1/Урок_6_Задание_1.py
@@ -0,0 +1,11 @@
+﻿N = int(input("Введите количество чисел: "))
+
+count_zeros = 0
+
+# Проходим по каждому числу
+for _ in range(N):
+    number = int(input("Введите число: "))
+    if number == 0:
+        count_zeros += 1
+
+print("Количество нулей:", count_zeros)
